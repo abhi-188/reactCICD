a6323d10a82b64d2aa8d1c2ad056bd1f0ef3ae6e
Abhishek Dubey <Abhishek.Dubey2@citiustech.com>
2023-04-08 16:27:12 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1> Hello World </h1>
-      <h2> My name is Abdelali Jadelmoula </h2>
-<h3> I am in love with devops (CI/CD) pipeline </h3>
+          <h1> Hello World </h1>
+          <h1> React App Using Jenkins and Docker</h1>
+          <h1>This React app is running on Docker Container</h1>
         </p>
         <a
           className="App-link"
